b475cadffa99e0ddc0edcebaeb2deb337c44a684
Megha Gulati <meghagulati30@gmail.com>
2013-08-02 21:42:58 -0700

    fixing css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :http_authenticate if Rails.env == "production"
+
+  def http_authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "codefellows" && password == "c0defe!!@ws"
+    end
+  end
 end
